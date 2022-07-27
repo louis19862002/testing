1a5daedd3ed575beb76701ac1f71854a30ade736
louis19862002 <96225981+louis19862002@users.noreply.github.com>
2022-07-27 11:44:50 -0700

    Delete Testing.R

--- Testing.R
+++
@@ -1,7 +0,0 @@
-print("This file was created within RStudio")
-print("And now it lives on GitHub")
-print("This file was created within RStudio")
-print("And now it lives on GitHub")
-print("And now it lives on GitHub")
-print("This file was created within RStudio")
-savehistory("~/Testing/Testing.R")
